8cb32d04d33c1aed815092dbc458e352592a84dc
IgnoreLicensesCN <IgnoreLicensesCN@users.noreply.github.com>
2025-01-29 17:50:25 +0800

    forgot to upload

--- src/main/java/io/github/ignorelicensescn/removedbysfcninspection/RemovedBySFCN.java
+++ src/main/java/io/github/ignorelicensescn/removedbysfcninspection/RemovedBySFCN.java
@@ -1,4 +1,15 @@
 package io.github.ignorelicensescn.removedbysfcninspection;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.TYPE;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value={CONSTRUCTOR, FIELD, LOCAL_VARIABLE, METHOD, PACKAGE, MODULE, PARAMETER, TYPE})
 public @interface RemovedBySFCN {
+    String since() default "";
+    boolean forRemoval() default false;
 }
